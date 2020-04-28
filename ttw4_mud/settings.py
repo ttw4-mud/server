26d6efa6448ad84dcaed7eabb7d6e017c6f874e9
nathan-saygers <nathan.saygers@gmail.com>
2020-04-28 12:46:05 -0700

    Install django rest framework
    Co-authored-by: Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>

--- ttw4_mud/settings.py
+++ ttw4_mud/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "whitenoise",
     "corsheaders",
+    "rest_framework",
+    "rest_framework.authtoken"
 ]
 
 MIDDLEWARE = [
@@ -115,6 +117,14 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
 
 ############################################################
 #   INTERNATIONALIZATION
