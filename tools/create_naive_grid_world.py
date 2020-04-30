fa1f549e1b26a80b8c4e0780ccc90a80f2d18635
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-30 17:16:00 -0400

    tools/create_naive_grid_world: start def
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>

--- tools/create_naive_grid_world.py
+++ tools/create_naive_grid_world.py
@@ -1,10 +1,35 @@
 ############################################################
 
 from adventure.models import Player, Tile
+from tools.tile_drawing import (
+    pixel_rows_of_tile,
+    pixel_rows_of_tile_list,
+)
 
 ############################################################
 
 
-def create_naive_grid_world():
+def print_tile(tile):
 
-    pass
+    print("\n".join(pixel_rows_of_tile(tile)))
+
+
+def print_tile_row(tile_row):
+
+    print("\n".join(pixel_rows_of_tile_list(tile_row)))
+
+
+def create_naive_grid_world(n_rows, n_cols):
+
+    # DEFINE TILES
+    tile_grid = [[Tile() for c in range(n_cols)] for r in range(n_rows)]
+
+    # PRINT TILES
+    print()
+
+    for r in range(len(tile_grid)):
+        print_tile_row(tile_grid[r])
+
+    print()
+
+    return
