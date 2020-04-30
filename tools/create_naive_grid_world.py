d2862b2b666d529fe6829c33eb0fbf3ab170889e
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-30 18:38:54 -0400

    create_naive_grid_world: continue def
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>

--- tools/create_naive_grid_world.py
+++ tools/create_naive_grid_world.py
@@ -1,35 +1,36 @@
 ############################################################
 
-from adventure.models import Player, Tile
-from tools.tile_drawing import (
-    pixel_rows_of_tile,
-    pixel_rows_of_tile_list,
-)
+from adventure.models import sides, Tile, Player
+from tools.tile_drawing import draw_tile_grid
 
 ############################################################
-
-
-def print_tile(tile):
-
-    print("\n".join(pixel_rows_of_tile(tile)))
-
-
-def print_tile_row(tile_row):
-
-    print("\n".join(pixel_rows_of_tile_list(tile_row)))
 
 
 def create_naive_grid_world(n_rows, n_cols):
 
     # DEFINE TILES
+
     tile_grid = [[Tile() for c in range(n_cols)] for r in range(n_rows)]
 
+    # CONNECTION TILES
+
+    for r in range(n_rows):
+
+        for c in range(n_cols):
+
+            # randomly choose to connect to adjacent tiles
+            # need to connect BOTH ways
+            # if tile in first row, can't go north
+            # if tile in first col, can't go west
+            # if tile in last row, can't go south
+            # if tile in last col, can't go east
+            # each tile must have at least 1 connection
+            pass
+
     # PRINT TILES
+
     print()
-
-    for r in range(len(tile_grid)):
-        print_tile_row(tile_grid[r])
-
+    print(draw_tile_grid(tile_grid))
     print()
 
     # START ALL PLAYERS
