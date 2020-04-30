b92871acff18dc1a685161f7ef59788c8f4c9b3f
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-30 19:48:17 -0400

    grid_world_tools: start defs of helpers
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>

---
+++ tools/grid_world_tools.py
@@ -0,0 +1,48 @@
+############################################################
+
+import random
+
+from adventure.models import sides
+
+############################################################
+
+
+def connect_from_to(direction, from_tile, to_tile):
+    """
+    Connect from one tile to another tile in the given direction.
+    """
+    pass
+
+
+def get_connected_sides(tile, row, col):
+    """
+    Get a list of connected sides of the tile.
+    True -> connected
+    False -> not connected
+    """
+    pass
+
+
+def get_locked_sides(tile, row, col):
+    """
+    Get a list of locked sides of the tile. Currently connected sides are locked.
+    True -> locked
+    False -> not locked
+    """
+    pass
+
+
+def generate_new_sides(tile, row, col):
+    """
+    Generate a list of which sides to connect. Currently locked sides cannot be connected.
+    True -> to be connected
+    False -> not to be connected
+    """
+    pass
+
+
+def has_any_connected_sides(tile, row, col):
+    """
+    Test if a tile has _any_ connected sides.
+    """
+    pass
