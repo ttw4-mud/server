6714cac507605cc5b025460c68cc1108096cb00d
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-04-30 21:00:14 -0400

    grid_world_tools: rename to indicate bools
    Co-authored-by: Nathan Saygers <48068572+nathan-saygers@users.noreply.github.com>

--- tools/grid_world_tools.py
+++ tools/grid_world_tools.py
@@ -7,7 +7,7 @@ from adventure.models import sides, Tile
 ############################################################
 
 
-def get_connected_sides(tile, row=None, col=None, grid_size=(None, None)):
+def get_connected_bool_sides(tile, row=None, col=None, grid_size=(None, None)):
     """
     Get a list of connected sides of the tile.
     True -> connected
@@ -22,10 +22,10 @@ def has_any_connected_sides(tile, row=None, col=None, grid_size=(None, None)):
     Test if a tile has _any_ connected sides.
     """
 
-    return any(get_connected_sides(tile, row, col, grid_size))
+    return any(get_connected_bool_sides(tile, row, col, grid_size))
 
 
-def get_locked_sides(tile, row, col, grid_size):
+def get_locked_bool_sides(tile, row, col, grid_size):
     """
     Get a list of locked sides of the tile. Currently connected sides are locked.
     True -> locked
@@ -33,7 +33,7 @@ def get_locked_sides(tile, row, col, grid_size):
     """
 
     # currently connected sides are necessarily locked
-    locked_sides = get_connected_sides(tile, row, col, grid_size)
+    locked_sides = get_connected_bool_sides(tile, row, col, grid_size)
 
     # if tile in first row, can't go north
     if row <= 0:
@@ -54,7 +54,7 @@ def get_locked_sides(tile, row, col, grid_size):
     return locked_sides
 
 
-def generate_new_sides(tile, row, col, grid_size):
+def generate_new_bool_sides(tile, row, col, grid_size):
     """
     Generate a list of which sides to newly connect.
     May generate no new connections.
@@ -63,7 +63,7 @@ def generate_new_sides(tile, row, col, grid_size):
     False -> not to be connected
     """
 
-    locked_sides = get_locked_sides(tile, row, col, grid_size)
+    locked_sides = get_locked_bool_sides(tile, row, col, grid_size)
 
     # if side is locked (True): no new side (False)
     # else: generate random True or False
@@ -73,7 +73,7 @@ def generate_new_sides(tile, row, col, grid_size):
     return new_sides
 
 
-def always_generate_new_sides(tile, row, col, grid_size, min_new_sides=1):
+def always_generate_new_bool_sides(tile, row, col, grid_size, min_new_sides=1):
     """
     Generate a list of which sides to newly connect.
     Always generates new sides (`min_new_sides`, default 1)
@@ -84,7 +84,7 @@ def always_generate_new_sides(tile, row, col, grid_size, min_new_sides=1):
     """
 
     count_all_sides = len(sides.keys())
-    count_locked_sides = sum(get_locked_sides(tile, row, col, grid_size))
+    count_locked_sides = sum(get_locked_bool_sides(tile, row, col, grid_size))
     max_new_sides = count_all_sides - count_locked_sides
 
     # adjust min_new_sides if there are too few unlocked sides available
@@ -96,7 +96,7 @@ def always_generate_new_sides(tile, row, col, grid_size, min_new_sides=1):
 
     while count_new_sides is None or count_new_sides < min_new_sides:
         # try to generate enough new sides
-        new_sides = generate_new_sides(tile, row, col, grid_size)
+        new_sides = generate_new_bool_sides(tile, row, col, grid_size)
         # we can't count the already locked sides as "new"
         count_new_sides = sum(new_sides) - count_locked_sides
 
